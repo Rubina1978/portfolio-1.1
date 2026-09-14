89b1d37581f6345fda9becaf6a843ec557adc151
Rubina1978 <hrubesova@gmail.com>
2026-09-14 02:01:17 +0100

    style buttons

--- home_page/models.py
+++ home_page/models.py
@@ -11,3 +11,19 @@ class Project(models.Model):
     technologies = models.CharField(max_length=250)
     github_url = models.URLField()
     live_url = models.URLField()
+
+
+class Skill(models.Model):
+    icon = models.ImageField(upload_to="skills")
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    order = models.PositiveIntegerField(default=0)
+
+
+def __str__(self):
+    return self.name
+
+
+class Meta:
+    verbose_name = 'Skill'
+    verbose_name_plural = 'Skills'
